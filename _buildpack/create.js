03d1e943321e0663659610b4fac0c45bd0226e28
James Zetlen <jzetlen@adobe.com>
2019-10-10 05:53:37 -0500

    feat: refactor create and add webpack composition

--- _buildpack/create.js
+++ _buildpack/create.js
@@ -3,20 +3,29 @@ const { resolve } = require("path");
 function createMe({ fs, tasks, options, findPackageRoot }) {
   return {
     async before() {
-      return tasks.CREATE({
+      return tasks.Create({
         template: "@magento/venia-concept"
       });
     },
     visitor: {
-      "package.json": ({ targetPath }) => {
-        const pkg = fs.readJsonSync(targetPath);
-        pkg.description =
+      "package.json": tasks.EditJson(({ target }) => {
+        target.description =
           "A new project based on a local copy of @magento/venia-ui";
-        fs.outputJsonSync(targetPath, pkg, {
-          spaces: 2
-        });
-      },
-      "webpack.config.js": tasks.COPY
+        return target;
+      }),
+      "webpack.config.js": async (opts, ...args) => {
+        const { targetPath, options } = opts;
+        // move the original out of the way
+        await fs.move(
+          targetPath,
+          resolve(options.directory, "webpack.venia.config.js"),
+          {
+            overwrite: true
+          }
+        );
+        // put ours in which calls the original from that path
+        tasks.Copy(opts, ...args);
+      }
     },
     async after() {
       const toCopyFromVeniaLib = [
@@ -30,9 +39,15 @@ function createMe({ fs, tasks, options, findPackageRoot }) {
         toCopyFromVeniaLib.map(veniaAsset =>
           fs.copy(
             resolve(uiDepLocation, veniaAsset),
-            resolve(options.directory, "venia-ui", veniaAsset),
+            resolve(
+              options.directory,
+              "src",
+              "@magento",
+              "venia-ui",
+              veniaAsset
+            ),
             {
-              overwrite: false,
+              overwrite: true,
               dereference: true,
               preserveTimestamps: true
             }
